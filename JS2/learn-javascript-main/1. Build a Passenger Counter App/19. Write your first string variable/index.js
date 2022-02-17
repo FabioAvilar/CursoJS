c6c18a697af071808f500bdbc2c0ca89ee14518e
DeboraDawidiuk <49881203+FabioAvilar@users.noreply.github.com>
2022-02-17 18:23:16 +0100

    :+1:

---
+++ JS2/learn-javascript-main/1. Build a Passenger Counter App/19. Write your first string variable/index.js
@@ -0,0 +1,7 @@
+let username = 'Fabio'
+
+let message = "You have tree new notifications"
+
+let messageTpUser = `${message}, ${username}!`
+
+console.log(messageTpUser)
---
+++ JS2/learn-javascript-main/1. Build a Passenger Counter App/2. Let's build a passenger counter app/index.js
@@ -0,0 +1,15 @@
+//document.getElementById('count-el').innerText = 5
+let countEl = document.getElementById("count-el")
+
+let count = 0
+
+ function  increment() {
+     count += 1
+     countEl.innerText = count
+ }
+
+function save() {
+    console.log(count)
+}
+
+save()
