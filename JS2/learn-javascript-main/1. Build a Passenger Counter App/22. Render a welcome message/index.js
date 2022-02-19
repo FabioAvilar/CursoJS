2f66d43e2fa98e95b35f7921817fd1a1919b318c
DeboraDawidiuk <49881203+FabioAvilar@users.noreply.github.com>
2022-02-19 19:41:09 +0100

    .

--- JS2/learn-javascript-main/1. Build a Passenger Counter App/22. Render a welcome message/index.js
+++ JS2/learn-javascript-main/1. Build a Passenger Counter App/22. Render a welcome message/index.js
@@ -1,6 +1,11 @@
 // Grab the welcome-el paragraph and store it in a variable called welcomeEl
 
+let welcomeEl = document.getElementById("welcome-el")
 // Create two variables (name & greeting) that contains your name
 // and the greeting we want to render on the page
+let name = "Fabio Bassani"
+let greeting = "Welcome Back "
 
 // Render the welcome message using welcomeEl.innerText
+
+welcomeEl.innerText = greeting + name
