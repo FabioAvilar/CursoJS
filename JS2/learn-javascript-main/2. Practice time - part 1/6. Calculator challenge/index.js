04f0dda7b9e0038254b349dd06b5dc487c52f233
FabioBassani <avilar.fabio@yahoo.com.br>
2022-02-26 11:50:35 +0100

    .

--- JS2/learn-javascript-main/2. Practice time - part 1/6. Calculator challenge/index.js
+++ JS2/learn-javascript-main/2. Practice time - part 1/6. Calculator challenge/index.js
@@ -3,7 +3,30 @@ let num2 = 2
 document.getElementById("num1-el").textContent = num1
 document.getElementById("num2-el").textContent = num2
 
+let sumEl = document.getElementById("sum-el")
+let result = num1 + num2
 // Create four functions: add(), subtract(), divide(), multiply()
+
+
+function add() {
+    let result = num1 + num2
+    sumEl.textContent = "Sum: " + result
+}
+
+function subtract() {
+    let result = num1 - num2
+    sumEl.textContent = "Sum: " + result
+}
+
+function divide() {
+    let result = num1 / num2
+    sumEl.textContent = "Sum: " + result
+}
+
+function multiply() {
+    let result = num1 * num2
+    sumEl.textContent = "Sum: " + result
+}
 // Call the correct function when the user clicks on one of the buttons
 // Perform the given calculation using num1 and num2
 // Render the result of the calculation in the paragraph with id="sum-el"
