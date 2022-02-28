6fd21fe43e7078da476a1ed20f40db14806dda65
FabioBassani <avilar.fabio@yahoo.com.br>
2022-02-28 20:18:26 +0100

    .

---
+++ JS2/curso.js
@@ -0,0 +1,10 @@
+var obj = {
+    showContext: function showContext() {
+        console.log('teste');
+    },
+    log: function log(value) {
+        console.log(value);
+    }
+};
+
+obj.showContext();
