128d58ec32ab11e7b682316c3d915fc7cee5c65b
FabioBassani <avilar.fabio@yahoo.com.br>
2022-03-20 12:44:09 +0100

    .

--- JS2/learn-javascript-main/3. Build a Blackjack game/22. Array indexes/index.js
+++ JS2/learn-javascript-main/3. Build a Blackjack game/22. Array indexes/index.js
@@ -1,10 +1,13 @@
 // Arrays - ordered lists of items
 
+// 0 indexed
 let featuredPosts = [
     "Check out my Netflix clone",
     "Here's the code for my project",
     "I've just relaunched my portfolio"
 ]
+
+// console.log( featuredPosts[2] )
 
 
 let experience = ["CEO at Scrimba", "Frontend developer at Xeneta", "People counter for Norstat"]
@@ -13,3 +16,7 @@ let experience = ["CEO at Scrimba", "Frontend developer at Xeneta", "People coun
 // Frontend developer at Xeneta
 // People counter for Norstat
 // CEO at Scrimba
+
+console.log(experience[1])
+console.log(experience[2])
+console.log(experience[0])
