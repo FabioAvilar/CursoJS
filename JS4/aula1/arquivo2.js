90a21d2821ac4bcc27f6cf31d03ab4d9165dc306
FabioBassani <avilar.fabio@yahoo.com.br>
2022-05-05 17:19:50 +0200

    basico em js

---
+++ JS4/aula1/arquivo2.js
@@ -0,0 +1,23 @@
+// const valorIngressoAdulto = 20
+
+// console.log(valorIngressoAdulto)
+
+// let nome = 'Rafael'
+// let idade = 25
+// let estaAprovado = true
+// let sobrenome = undefined
+// let corSelecionado = null
+
+// let pessoa = {
+//     nome: 'Rafael',
+//     altura: 1.68
+// };
+
+// // console.log(typeof(corSelecionado))
+// console.log(pessoa)
+
+// Arrays
+
+let familia = [26,45,'fabio',17]
+console.log(familia.length)
+console.log(familia[2])
