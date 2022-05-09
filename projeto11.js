1ce87f2e218c4b4680618fd4c9fd420ce31fb351
FabioBassani <avilar.fabio@yahoo.com.br>
2022-05-09 17:29:56 +0200

    ,

---
+++ projeto11.js
@@ -0,0 +1,13 @@
+
+
+function Celular(marcaCelular, tamanhoTela,capacidadeBateria){
+    this.marcaCelular = marcaCelular,
+    this.tamanhoTela = tamanhoTela,
+    this.capacidadeBateria = capacidadeBateria,
+    this.ligar = function(){
+        console.log("Fazendo ligaçao...")
+    }
+}
+
+const celular = new Celular('asus',5.5,5000)
+console.log(celular)
